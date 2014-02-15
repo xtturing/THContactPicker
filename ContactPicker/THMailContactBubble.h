4d750a36bdd2d20e1d0b889a10953280b1f0b206
xtturing <xtturing@gmail.com>
2014-02-15 18:11:10 +0800

    针对邮件系统的添加联系人修改，框架搭建

---
+++ ContactPicker/THMailContactBubble.h
@@ -0,0 +1,13 @@
+//
+//  THMailContactBubble.h
+//  ContactPicker
+//
+//  Created by xtturing on 14-2-15.
+//  Copyright (c) 2014年 Tristan Himmelman. All rights reserved.
+//
+
+#import "THContactBubble.h"
+
+@interface THMailContactBubble : THContactBubble
+
+@end
